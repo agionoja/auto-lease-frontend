056cbe49c99b5a4b5a898d232a3dd3de50cfe820
agionoja <pauldivineagionoja@gmail.com>
2024-07-12 11:45:45 +0100

    dld

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import authImg from "../../assets/images/auth.png";
+import { NavLink } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,6 +13,14 @@ export default function Index() {
   return (
     <div className="font-sans p-4">
       <h1 className={"bg-red-300 animate-bounce "}>This is the home page</h1>
+
+      <nav>
+        <NavLink to={"/auth/register"}>Register</NavLink>
+        <NavLink to={"/auth/login"}>Login</NavLink>
+        <NavLink to={"/auth/2fa/login"}>Two Step Authentication</NavLink>
+        <NavLink to={"/auth/request/Token"}>Request Token</NavLink>
+        <NavLink to={"/auth/verify/"}>Verify Account</NavLink>
+      </nav>
     </div>
   );
 }
