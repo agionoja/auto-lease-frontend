3966da6dcddd4b589211965d034715a1a6ed6f5f
agionoja <pauldivineagionoja@gmail.com>
2024-07-12 11:28:06 +0100

    x

--- app/routes/_a.auth.2fa.login/route.tsx
+++ app/routes/_a.auth.2fa.login/route.tsx
@@ -5,15 +5,15 @@ import fetchClient from "~/utils/fetchClient";
 import { LabelInput } from "~/components/label-input";
 
 export async function action({ request }: ActionFunctionArgs) {
-  const { email } = Object.fromEntries(await request.formData());
+  const { otp } = Object.fromEntries(await request.formData());
 
-  const res = await fetchClient(`/auth/request/token`, {
+  const res = await fetchClient(`/auth/2fa/`, {
     method: "PATCH",
-    body: JSON.stringify({ email }),
+    body: JSON.stringify({ otp }),
   });
 
   console.log(res);
-  if (res.statusText === "success") redirect("/");
+  if (res.statusText === "success") return redirect("/");
   return json({ message: res.message });
 }
 
@@ -33,17 +33,17 @@ export default function RequestToken() {
   return (
     <Form ref={formRef} className={"flex flex-col gap-8 "} method={"POST"}>
       <LabelInput
-        label={"Email Address"}
-        type={"email"}
-        name={"email"}
-        placeholder={"Email"}
+        label={"Enter your OTP"}
+        type={"text"}
+        name={"otp"}
+        placeholder={"One Time Password"}
       />
       <button
         type="submit"
         disabled={isSubmitting}
         className={`bg-black text-white py-3 rounded-lg ${isSubmitting && "opacity-50"} ${isSubmitting && "cursor-not-allowed"}`}
       >
-        {isSubmitting ? "Requesting..." : "Request Verification Token"}
+        {isSubmitting ? "Verifying..." : "Enter"}
       </button>
       {actionData?.message && (
         <div className="text-red-500">{actionData.message}</div>
